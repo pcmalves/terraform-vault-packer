1159f53291195fa7fe912355cd9717c116003be9
Paulo Cezar Martins Alves <paulo.alves@redealumni.com>
2020-01-23 23:00:00 -0300

    update file json

--- output.tf
+++ output.tf
@@ -1,7 +1,7 @@
-output "vpc_id" {
-  value = "${module.vault-vpc.vpc_id}"
-}
+# output "vpc_id" {
+#   value = "${module.vault-vpc.vpc_id}"
+# }
+# output "subnet_id" {
+#   value = "${module.vault-vpc.subnet_id}"
+# }
 
-output "subnet_id" {
-  value = "${module.vault-vpc.subnet_id}"
-}
