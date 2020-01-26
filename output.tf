773486cbd8a19d4f99b7567734ec7075c8141eea
Paulo Cezar Martins Alves <paulo.alves@redealumni.com>
2020-01-26 15:14:45 -0300

    refactory network project and create ami with packer

--- output.tf
+++
@@ -1,7 +0,0 @@
-# output "vpc_id" {
-#   value = "${module.vault-vpc.vpc_id}"
-# }
-# output "subnet_id" {
-#   value = "${module.vault-vpc.subnet_id}"
-# }
-
---
+++ packer/config.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+DEBIAN_FRONTEND=noninteractive 
+sudo apt-get update && sudo apt-get -y install curl unzip 
