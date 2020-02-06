3649ec9dfe6c167dd6c3db7a342d4f1a3697750f
Paulo Cezar Martins Alves <paulo.alves@redealumni.com>
2020-02-05 22:24:29 -0300

    adjust subnet private and subnet public

--- variables.tf
+++ variables.tf
@@ -10,12 +10,8 @@ variable "enable_dns_hostnames" {
   default = true
 }
 
-variable "subnet_cidr" {
-  default = "10.0.1.0/24"
-}
-
 variable "vpc_cidr_block" {
-  default = "10.0.0.0/16"
+  default = "172.16.0.0/16"
 }
 
 variable "destination_cidr_block" {
@@ -23,15 +19,15 @@ variable "destination_cidr_block" {
 }
 
 variable "subnet-public" {
-  default = "10.0.0.0/24"
+  default = ["172.16.3.0/24", "172.16.0.0/24"]
 }
 
 variable "subnet-private" {
-  default = "10.0.1.0/24"
+  default = ["172.16.2.0/24", "172.16.1.0/24"]
 }
 
 variable "availability_zone" {
-  default = "us-west-1c"
+  default = ["us-west-1a", "us-west-1c"]
 }
 
 variable "map_public_ip_on_launch" {
